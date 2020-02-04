7e784f3fb53dcf514e2223cfdbbc2326bfbf6046
Andrei Todosi <andreitodosi88@gmail.com>
2020-02-04 16:30:48 +0000

    Ravenous Part 3 + Comments

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -20,13 +20,19 @@ const business = {
 const businesses = [business, business, business, business, business, business];
 
 class App extends React.Component {
+  searchYelp(term, location, sortBy) {
+    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+  }
+
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
-        <SearchBar />
+        <SearchBar searchYelp={this.searchYelp} />
         <BusinessList businesses={businesses}>
           <hr />
+
+          {/* This will render the children of Business */}
           {this.props.children}
         </BusinessList>
       </div>
